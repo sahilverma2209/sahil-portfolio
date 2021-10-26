0bc3b3db9b8abacd29770c7490d7e0c8d8ecd3de
Sahil Verma <sahilverma.0922@gmail.com>
2021-10-26 13:54:50 -0400

    Updates

---
+++ precache-manifest.5367641d320ef6b0a2bd4c07b23e6203.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9092d06179a34b246ab7201233114b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4229fe84f92b51ae7e2e",
+    "url": "/static/css/main.6a1f9f72.chunk.css"
+  },
+  {
+    "revision": "edde9d8954b1285c6a46",
+    "url": "/static/js/2.144046d4.chunk.js"
+  },
+  {
+    "revision": "4229fe84f92b51ae7e2e",
+    "url": "/static/js/main.9a39593e.chunk.js"
+  },
+  {
+    "revision": "ea4fc188b8acc5525267",
+    "url": "/static/js/runtime~main.f0ee57a3.js"
+  },
+  {
+    "revision": "183de736efe789394f26ed013b0b5cdd",
+    "url": "/static/media/Calibre-Light.183de736.woff2"
+  },
+  {
+    "revision": "46b36969c7bb1d7ed4c8253e8f274788",
+    "url": "/static/media/Calibre-Medium.46b36969.woff2"
+  },
+  {
+    "revision": "b63c62e591d0c8fbe2c8f009883346d5",
+    "url": "/static/media/Calibre-Regular.b63c62e5.woff2"
+  },
+  {
+    "revision": "d8b856473a51d7eec2a9bccf2fbea54c",
+    "url": "/static/media/Calibre-Semibold.d8b85647.woff2"
+  },
+  {
+    "revision": "99497ab65b5b8a7e5c18b7b3edde5c85",
+    "url": "/static/media/SFMono-Medium.99497ab6.woff2"
+  },
+  {
+    "revision": "7faa66605357721067fe1b07d9103015",
+    "url": "/static/media/SFMono-Regular.7faa6660.woff2"
+  },
+  {
+    "revision": "eb23f13f16a5ffd3a88da2998df14654",
+    "url": "/static/media/adi.eb23f13f.png"
+  },
+  {
+    "revision": "aa14b5cdcdb65fbafcf2e606c393c156",
+    "url": "/static/media/aliste.aa14b5cd.png"
+  },
+  {
+    "revision": "ad999cd6956f028252e72d28afd757f3",
+    "url": "/static/media/datepicker.ad999cd6.png"
+  },
+  {
+    "revision": "4ec255d0e21dcfa1322b492d7116bb70",
+    "url": "/static/media/git.4ec255d0.png"
+  },
+  {
+    "revision": "97557d4b6b76d317a5b626fb63203a6e",
+    "url": "/static/media/jba.97557d4b.png"
+  },
+  {
+    "revision": "ce153a3fff66fd8e178a385628b866e3",
+    "url": "/static/media/pagination.ce153a3f.png"
+  },
+  {
+    "revision": "a54b35d526ea6bfaa7ac16ae15a5fdaf",
+    "url": "/static/media/sahil.a54b35d5.jpeg"
+  },
+  {
+    "revision": "3e281097be7cd25f27a6418c4b9a4dbf",
+    "url": "/static/media/signa.3e281097.png"
+  },
+  {
+    "revision": "6de3afc906bb0a86aa9e5c0222294241",
+    "url": "/static/media/vh.6de3afc9.png"
+  },
+  {
+    "revision": "d356becfde4922f30ef8b7cfbe1c337a",
+    "url": "/static/media/wheelpicker.d356becf.png"
+  },
+  {
+    "revision": "54942e900ba3325399873bddc402429d",
+    "url": "/static/media/yelp.54942e90.png"
+  }
+]);
